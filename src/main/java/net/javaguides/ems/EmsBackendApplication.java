38832abd168f2b89780c45660cb4d228d78d4f3c
revin99 <revingohil@gmail.com>
2025-07-08 19:53:14 +0530

    Setup MYSQL connectivity in spring

--- src/main/java/net/javaguides/ems/EmsBackendApplication.java
+++ src/main/java/net/javaguides/ems/EmsBackendApplication.java
@@ -7,7 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmsBackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EmsBackendApplication.class, args);
+
+		Student student = new Student(1,"revin");
+		System.out.println(student.getId());
+		System.out.println(student.getName());
+
+
+
 	}
 
 }
---
+++ src/main/java/net/javaguides/ems/Student.java
@@ -0,0 +1,16 @@
+package net.javaguides.ems;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.SpringApplication;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    private int id;
+    private String name;
+}
